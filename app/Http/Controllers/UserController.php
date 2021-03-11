38e619ff0a7081c7eba0f3d1cf4aedc4235520a9
DRAGON <longbaonguyen97@gmail.com>
2021-03-11 23:26:56 +0700

    validate input before create user api

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\StorePostRequest;
 use App\Models\User;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -12,17 +14,20 @@ class UserController extends Controller
      * get all users
      * @return object
      */
-    public function index(){
+    public function index()
+    {
         return User::all();
     }
+
     /**
      * get detail user
      * @param $id
      * @return array
      */
-    public function show($id){
-        $users =User::find($id);
-        if (empty($users)){
+    public function show($id)
+    {
+        $users = User::find($id);
+        if (empty($users)) {
             return [
                 'code'=>0,
                 'data'=>[]
@@ -48,9 +53,56 @@ class UserController extends Controller
      * @param Request $request
      * @return mixed
      */
-    public function store(Request $request)
+    public function store(StorePostRequest $request)
     {
-        return User::create($request->all());
+//        $validator = Validator::make($request->all(),
+//            [
+//                'first_name' => 'required|max:255',
+//                'last_name' => 'required',
+//                'email' => 'unique:users,email|required|regex:/^.+@.+$/i'
+//            ],
+//            [
+//                'first_name.required' => 'Loi ne',
+//                'email.regex' => 'Email sai roi',
+//                'email.unique' => 'Email da ton tai'
+//            ]
+//        );
+//        if ($validator->fails()) {
+//
+//            return [
+//                'code'=>0,
+//                'data'=>[
+//                    'error'=>$validator->errors()
+//                ]
+//            ];
+//        } else {
+//            DB::table('users')->insert([
+//                'first_name' => $request->get('first_name'),
+//                'last_name' => $request->get('last_name'),
+//                'email' => $request->get('email'),
+//                'password' => Hash::make($request->get('password'))
+//            ]);
+//
+//            return [
+//                'code' => 1
+//            ];
+//        }
+
+        $user = User::create([
+            "first_name" => $request->input('first_name'),
+            "last_name" => $request->input('last_name'),
+            "password" => Hash::make($request->input('password')),
+            "email" => $request->input('email')
+        ]);
+
+        return [
+            'code' => 1,
+            'data' => [
+                'success' => "Created successfuly",
+                'user' => response($user)
+            ]
+        ];
+
 
     }
 
---
+++ app/Http/Requests/StorePostRequest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+//use Illuminate\Support\Facades\Validator;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Http\Exceptions\HttpResponseException;
+class StorePostRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'first_name' => 'required|max:255',
+            'last_name' => 'required',
+            'email' => 'unique:users,email|required|regex:/^.+@.+$/i',
+            'password'=>'required'
+        ];
+    }
+    public function messages()
+    {
+        return [
+            'first_name.required' => 'First name is required!',
+            'last_name.required' => 'Last name is required!',
+            'email.regex' => 'The format email is not correct',
+            'email.unique' => 'The email has been existed',
+        ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+
+        $errors = (new ValidationException($validator))->errors();
+        throw new HttpResponseException(response()->json(
+            [
+                'code'=>0,
+                'error' => $errors
+            ]));
+    }
+}
